c8e3ee1e6376ed47004e844660f2212e7a635ba6
Leah Rumancik <leah.rumancik@gmail.com>
2022-07-26 14:59:26 -0700

    ltm: reboot hung tests and resume

--- test-appliance/files/usr/local/lib/gce-server/ltm/shard.go
+++ test-appliance/files/usr/local/lib/gce-server/ltm/shard.go
@@ -38,6 +38,7 @@ type ShardWorker struct {
 	vmStatus    string
 	vmtestStart time.Time
 	testResult  server.ResultType
+	vmReset     bool
 
 	log                *logrus.Entry
 	logPath            string
@@ -48,9 +49,10 @@ type ShardWorker struct {
 }
 
 const (
-	monitorTimeout  = 1 * time.Hour
+	monitorTimeout  = 5 * time.Minute
 	noStatusTimeout = 10 * time.Minute
 	monitorInterval = 60 * time.Second
+	resetTimeout    = 10 * time.Minute
 	gsInterval      = 10 * time.Second
 	maxAttempts     = 5
 )
@@ -70,6 +72,7 @@ func NewShardWorker(sharder *ShardScheduler, shardID string, config string, zone
 		vmStatus:    "waiting for launch",
 		vmtestStart: time.Now(),
 		testResult:  server.DefaultResult,
+		vmReset:     false,
 
 		log:                sharder.log.WithField("shardID", shardID),
 		logPath:            logPath,
@@ -183,6 +186,7 @@ func (shard *ShardWorker) monitor() {
 				if *metaData.Value != shard.vmStatus {
 					shard.vmStatus = *metaData.Value
 					shard.vmtestStart = time.Now()
+					shard.vmReset = false
 					break
 				}
 			}
@@ -201,18 +205,23 @@ func (shard *ShardWorker) monitor() {
 			log.Debug("waiting to get test status metadata")
 		}
 
-		if time.Since(shard.vmtestStart) > monitorTimeout {
-			if !shard.sharder.keepDeadVM {
-				shard.shutdownOnTimeout(instanceInfo.Metadata)
-			}
-			shard.vmStatus = "timeout on one test"
-			shard.testResult = server.Hang
 
-			log.WithFields(logrus.Fields{
-				"status": shard.vmStatus,
-				"start":  shard.vmtestStart.Format(time.Stamp),
-			}).Errorf("Instance seems to have wedged, no status update for %s", monitorTimeout.Round(time.Minute))
+		if shard.vmReset && time.Since(shard.vmtestStart) > resetTimeout {
+			log.Errorf("VM did not come back online after reset, exiting");
 			return
+		}
+
+		if time.Since(shard.vmtestStart) > monitorTimeout && ! shard.vmReset {
+			log.Debug("Resetting VM")
+			err := shard.sharder.gce.ResetVM(shard.sharder.projID, shard.zone, shard.name)
+			if err != nil {
+				log.Errorf("Failed to reset %s", shard.name)
+				shard.vmStatus = "failed to reset after timeout"
+				shard.testResult = server.Error
+				return
+			}
+			shard.vmReset = true
+			shard.vmtestStart = time.Now()
 		}
 
 		log.WithFields(logrus.Fields{
